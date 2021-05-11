a181ac1432dd244f90bc11bb41c0397837c7e905
cryptosudo <cryptosudo@gmail.com>
2021-05-11 22:21:40 +0300

    Complus | SDK for Polygon - Matic

---
+++ README.md
@@ -0,0 +1,23 @@
+## COMPLUS | SDK FOR POLYGON-MATIC
+
+[![code style: prettier](https://img.shields.io/badge/code_style-prettier-ff69b4.svg?style=flat-square)](https://github.com/prettier/prettier)
+[![Actions Status](https://github.com/complusnetwork/complus-sdk-matic/workflows/CI/badge.svg)](https://github.com/complusnetwork/complus-sdk-matic)
+[![npm version](https://img.shields.io/npm/v/@complus/sdk-matic/latest.svg)](https://www.npmjs.com/package/@complus/sdk-matic/v/latest)
+[![npm bundle size (scoped version)](https://img.shields.io/bundlephobia/minzip/@complus/sdk-matic/latest.svg)](https://bundlephobia.com/result?p=@complus/sdk-matic@latest)
+
+## POLYGON ( MATIC ) NETWORK
+
+- [POLYGON ( MATIC ) Docs](https://docs.matic.network)
+- [POLYGON ( MATIC ) Explorer](https://explorer-mainnet.maticvigil.com/)
+
+## Local Development
+
+The following assumes the use of `node@>=10`
+
+#### Install Dependencies
+
+`yarn`
+
+#### Run Build
+
+`yarn build`
---
+++ src/constants.ts
@@ -0,0 +1,47 @@
+import JSBI from 'jsbi'
+
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
+
+export enum ChainId {
+  MAINNET = 137,
+  TESTNET = 80001
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS = '0x973c934137DD687eca67Bdd1c5A8B74286964Ac6'
+
+export const INIT_CODE_HASH = '0x19e9217894cb03590fd45c96a15e3e5b93c3c728b92eba602117916436a26b64'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
+export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
